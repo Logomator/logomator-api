a27053f59aadd7ff29f48bdc9935a6b151370740
Hunter Gemmer <hunterg325@gmail.com>
2017-05-04 20:09:06 -0400

    init; added expressjs, the noun project, and config.json.

---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var config = require('./config.json');
+var NOUN_PROJECT_API_KEY = config.NOUN_PROJECT_API_KEY;
+var NOUN_PROJECT_API_SECRET = config.NOUN_PROJECT_API_SECRET;
+var cors = require('cors');
+var NounProject = require('the-noun-project'),
+    nounProject = new NounProject({
+        key: NOUN_PROJECT_API_KEY,
+        secret: NOUN_PROJECT_API_SECRET
+    });
+
+app.use(cors());
+
+app.get('/api/icons/:term', function (req, res) {
+
+    if (!req.params.term) {
+        return res.send({error: "You need to specify a term"});
+    }
+
+    nounProject.getIconsByTerm(req.params.term, {limit: 9}, function (err, data) {
+
+        if (!err) {
+            return res.send(data.icons);
+        }
+        return res.send({message: "API not available"});
+
+    });
+});
+
+app.listen(8000,  function () {
+    console.log('Logomator API listening on port 8000!')
+});
