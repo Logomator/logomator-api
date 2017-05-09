fc0cd541db2236b3d6f4bcc6c5c6d0fbb22b1ff4
Hunter Gemmer <hunterg325@gmail.com>
2017-05-09 16:26:19 -0400

    added airbnb .jshintrc and .eslint.js.

--- src/logo/logo.js
+++ src/logo/logo.js
@@ -1,7 +1,6 @@
-"use strict";
-
 const window = require('svgdom');
 const SVG = require('svg.js')(window);
+
 const document = window.document;
 
 // Logo configuration
@@ -10,31 +9,30 @@ const LOGO_HEIGHT = 230;
 const LOGO_BACKGROUND = './images/logo-background-shadow.png';
 
 class Logo {
-    constructor(companyName, tagline, fontFamily, companyNameColor, taglineColor, recipe) {
-        this.companyName = companyName || 'Dopest';
-        this.tagline = tagline || null;
-        this.fontFamily = fontFamily || 'Proxima Nova';
-        this.companyNameColor = companyNameColor || '#FF6600';
-        this.taglineColor = taglineColor || '#FF6600';
-        this.recipe = recipe;
+  constructor(companyName, tagline, fontFamily, companyNameColor, taglineColor, recipe) {
+    this.companyName = companyName || 'Dopest';
+    this.tagline = tagline || null;
+    this.fontFamily = fontFamily || 'Proxima Nova';
+    this.companyNameColor = companyNameColor || '#FF6600';
+    this.taglineColor = taglineColor || '#FF6600';
+    this.recipe = recipe;
+  }
 
-    }
-
-    generate() {
-        const draw = SVG(document.documentElement).size(300, 230);
-        draw.rect(LOGO_WIDTH, LOGO_HEIGHT).fill('#fff');
-        draw.image(LOGO_BACKGROUND, 300, 230);
-        draw.text(this.companyName).font({
-            fill: this.companyNameColor,
-            family: this.fontFamily,
-            size: '30',
-            anchor: this.recipe.companyNameAnchor,
-            leading: this.recipe.companyNameLeading,
-            dx: this.recipe.companyNameDx,
-            dy: this.recipe.companyNameDy
-        });
-        return draw.svg();
-    }
+  generate() {
+    const draw = SVG(document.documentElement).size(300, 230);
+    draw.rect(LOGO_WIDTH, LOGO_HEIGHT).fill('#fff');
+    draw.image(LOGO_BACKGROUND, 300, 230);
+    draw.text(this.companyName).font({
+      fill: this.companyNameColor,
+      family: this.fontFamily,
+      size: '30',
+      anchor: this.recipe.companyNameAnchor,
+      leading: this.recipe.companyNameLeading,
+      dx: this.recipe.companyNameDx,
+      dy: this.recipe.companyNameDy,
+    });
+    return draw.svg();
+  }
 }
 
 module.exports = Logo;
--- src/logo/recipes.js
+++ src/logo/recipes.js
@@ -1,16 +1,26 @@
 class Recipes {
-    getRecipes() {
-        return [
-            // Recipe 1
-            {
-                hasTagline: false,
-                companyNameAnchor: 'middle',
-                companyNameLeading: 4,
-                companyNameDy: '50%',
-                companyNameDx: '50%'
-            }
-        ]
-    }
+  getRecipes() {
+    return [
+      // Recipe 1
+      {
+        hasTagline: false,
+        companyNameAnchor: 'middle',
+        companyNameLeading: 4,
+        companyNameDy: '50%',
+        companyNameDx: '50%'
+      },
+
+      {
+        hasTagline: true,
+        companyNameAnchor: 'middle',
+        companyNameLeading: 4,
+        companyNameDy: '50%',
+        companyNameDx: '50%',
+        taglineDy: '60%',
+        taglineDx: '60%'
+      }
+    ]
+  }
 }
 
 module.exports = new Recipes();
