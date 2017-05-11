8d26c1e411c569a41a07e97139fd933b6a620208
Hunter Gemmer <hunterg325@gmail.com>
2017-05-10 23:29:39 -0400

    updated index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const config = require('./config/config.json');
 const cors = require('cors');
 const NounProject = require('the-noun-project');
-const router = express.Router();
 
 const app = express();
 const NOUN_PROJECT_API_KEY = config.NOUN_PROJECT_API_KEY;
@@ -24,29 +23,13 @@ app.get('/api/icons/:term', (req, res) => {
     return res.send({ error: 'You need to specify a term' });
   }
 
-  nounProject.getIconsByTerm(req.params.term, { limit: 9 }, function (err, data) {
-
+  nounProject.getIconsByTerm(req.params.term, { limit: 9 }, (err, data) => {
     if (!err) {
-      data.icons.forEach((icon) => {
-        console.log(icon.preview_url_84);
-      });
       return res.send(data.icons);
     }
-    return res.send({ message: "API not available" });
-
+    return res.send({ message: 'API not available' });
   });
 });
-
-
-const getIcons = () => {
-  const icons = [];
-  nounProject.getIconsByTerm('puppies', { limit: 9 }, (err, data) => {
-    if (!err) {
-      icons.push(data.icons);
-    }
-  });
-  return icons;
-};
 
 app.get('/logo', (req, res) => {
   const logos = [];
@@ -60,7 +43,7 @@ app.get('/logo', (req, res) => {
     logos.push(
       new Logo('Dope Logos Now', 'Puppies Galore', 'Proxima Nova', '#FF6600', '#818691', recipe, icons).generate());
   });
-  return res.send(logos[3]);
+  return res.send(logos);
 });
 
 app.listen(process.env.PORT || 8000, () => {
