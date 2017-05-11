a0877276fd8bb109b14575465dfe3f54a1ce8203
Hunter Gemmer <hunterg325@gmail.com>
2017-05-10 22:53:30 -0400

    save progress

--- index.js
+++ index.js
@@ -17,18 +17,24 @@ const recipes = require('./src/logo/recipes');
 app.use(cors());
 app.use(express.static('public'));
 
+
 app.get('/api/icons/:term', (req, res) => {
+
   if (!req.params.term) {
     return res.send({ error: 'You need to specify a term' });
   }
 
-  nounProject.getIconsByTerm(req.params.term, { limit: 9 }, (err, data) => {
+  nounProject.getIconsByTerm(req.params.term, { limit: 9 }, function (err, data) {
+
     if (!err) {
+      data.icons.forEach((icon) => {
+        console.log(icon.preview_url_84);
+      });
       return res.send(data.icons);
     }
-  });
+    return res.send({ message: "API not available" });
 
-  return res.send({ message: 'API not available' });
+  });
 });
 
 
@@ -44,18 +50,13 @@ const getIcons = () => {
 
 app.get('/logo', (req, res) => {
   const logos = [];
-
-  nounProject.getIconsByTerm('puppies', { limit: 9 }, (err, data) => {
-
-    if (!err) {
-      icons.push(data.icons);
-    }
-  });
-
-  console.log(getIcons());
-
+  const icons = [
+    'https://d30y9cdsu7xlg0.cloudfront.net/png/206102-84.png',
+    'https://d30y9cdsu7xlg0.cloudfront.net/png/318161-84.png',
+    'https://d30y9cdsu7xlg0.cloudfront.net/png/673953-84.png',
+    'https://d30y9cdsu7xlg0.cloudfront.net/png/991075-84.png',
+  ];
   recipes.getRecipes().forEach((recipe) => {
-
     logos.push(
       new Logo('Dope Logos Now', 'Puppies Galore', 'Proxima Nova', '#FF6600', '#818691', recipe, icons).generate());
   });
